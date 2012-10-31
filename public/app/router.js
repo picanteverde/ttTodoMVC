4b4863817a26dbbea8da559a3af3f51914bb71ac
Alejandro Hernández <picanteverde@gmail.com>
2012-10-31 00:02:22 -0300

    fixing login issue

--- public/app/router.js
+++ public/app/router.js
@@ -20,19 +20,7 @@ function(app, Login, Todos, Register) {
     index: function() {
       var router = this;
       if(!app.loggedIn){
-        $.ajax({
-          url: "/api/login",
-          type: "GET",
-          dataType: "json",
-          success: function(data){
-            if(data.auth){
-              router.navigate("/todos",{trigger: true});
-              app.loggedIn=data.auth;
-            }else{
-              router.navigate("/login",{trigger: true});
-            }
-          }
-        });
+        router.navigate("/login",{trigger: true});
       }else{
         this.navigate("/todos",{trigger: true});
       }
